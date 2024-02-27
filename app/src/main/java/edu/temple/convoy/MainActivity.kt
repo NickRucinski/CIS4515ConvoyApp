90ea85e920f6e107dedbc7a22c2d3f355b6f6beb
Nicholas Rucinski <tur45021@temple.edu>
2024-02-27 12:30:44 -0500

    fixed what happens when a user leaves a convoy after the host leaving

--- app/src/main/java/edu/temple/convoy/MainActivity.kt
+++ app/src/main/java/edu/temple/convoy/MainActivity.kt
@@ -234,7 +234,25 @@ class MainActivity : AppCompatActivity(), DashboardFragment.DashboardInterface, 
         Log.d("MainActivity Message Receiver", message.toString())
         if(message.getString("action") == "END" && convoyViewModel.getUserJoinedConvoy().value == true){
             runOnUiThread {
-                leaveConvoy()
+                Helper.api.leaveConvoy(
+                    this,
+                    Helper.user.get(this),
+                    Helper.user.getSessionKey(this)!!,
+                    Helper.user.getConvoyId(this)!!
+                ) { response ->
+                    if (Helper.api.isSuccess(response)) {
+                        convoyViewModel.setConvoyId("")
+                        convoyViewModel.setUserJoinedConvoy(null)
+                        Helper.user.clearConvoyId(this@MainActivity)
+                        Helper.user.clearJoinedState(this@MainActivity)
+                        stopLocationService()
+                    } else
+                        Toast.makeText(
+                            this@MainActivity,
+                            Helper.api.getErrorMessage(response),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                }
             }
         }
     }
