bf15c6f8a88380d9cee4550751c56a699226f783
Nicholas Rucinski <tur45021@temple.edu>
2024-03-11 20:43:34 -0400

    first pass at an audio player

--- app/src/main/java/edu/temple/convoy/AudioPlayer.kt
+++ app/src/main/java/edu/temple/convoy/AudioPlayer.kt
@@ -1,4 +1,23 @@
 package edu.temple.convoy
 
-class AudioPlayer {
+import android.content.Context
+import android.media.MediaPlayer
+import androidx.core.net.toUri
+import java.io.File
+
+class AudioPlayer(val context: Context) {
+
+    private var player: MediaPlayer? = null
+    fun play(file: File){
+        MediaPlayer.create(context, file.toUri()).apply{
+            player = this
+            start()
+        }
+    }
+
+    fun stop(){
+        player?.stop()
+        player?.release()
+        player = null
+    }
 }
