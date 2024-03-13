a5df226fa011a936abf2535239fbbd6a603366d8
Nicholas Rucinski <tur45021@temple.edu>
2024-03-13 11:03:09 -0400

    added the ability to download the file from the link

--- app/src/main/java/edu/temple/convoy/AudioPlayer.kt
+++ app/src/main/java/edu/temple/convoy/AudioPlayer.kt
@@ -2,10 +2,20 @@ package edu.temple.convoy
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.util.Log
 import androidx.core.net.toUri
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.io.BufferedInputStream
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.net.URL
+import java.time.LocalDateTime
 
-class AudioPlayer(val context: Context) {
+class AudioPlayer(val context: Context): FCMCallbackHelper.FCMCallback {
 
     private var player: MediaPlayer? = null
     fun play(file: File){
@@ -20,4 +30,43 @@ class AudioPlayer(val context: Context) {
         player?.release()
         player = null
     }
+
+    override fun messageReceived(message: JSONObject) {
+        if(message.getString("action") == "MESSAGE"){
+            val link = message.getString("message_file")
+            val audioFile = downloadAudio(link, message.getString("username"))
+            //I think I want to add this to a queue in the view model
+            play(audioFile)
+        }
+    }
+
+    fun downloadAudio(urlString: String, userName: String): File{
+        val url = URL(urlString)
+
+        val file = File(context.filesDir, "${userName}_Message_${LocalDateTime.now()}")
+        try{
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.d("Download","Starting File Download")
+                url.openStream().use { inp ->
+                    BufferedInputStream(inp).use { bis ->
+                        FileOutputStream(file).use { fos ->
+                            val data = ByteArray(1024)
+                            var count: Int
+                            while (bis.read(data, 0, 1).also { count = it } != -1) {
+                                fos.write(data, 0, count)
+                            }
+                        }
+                    }
+                }
+                Log.d("Download","File Finished Downloading")
+
+            }
+        } catch (e: IOException){
+            e.printStackTrace()
+        }
+        return file
+    }
+
 }
+
+data class AudioMessage(val file: File, val userName: String)
